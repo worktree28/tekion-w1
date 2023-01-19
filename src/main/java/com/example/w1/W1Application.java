5388d746c5f8755e155a43a3368fb87d81bac221
harsh mishra <hmishra2001@live.in>
2023-01-19 17:45:59 +0530

    intial

---
+++ src/main/java/com/example/w1/W1Application.java
@@ -0,0 +1,13 @@
+package com.example.w1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/w1/W1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.w1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
