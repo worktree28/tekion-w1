b986e9a5c5a4725d83589b36c720fd1403919a81
harsh mishra <hmishra2001@live.in>
2023-01-20 11:10:22 +0530

    added dependencies

--- src/main/java/com/example/w1/W1Application.java
+++ src/main/java/com/example/w1/W1Application.java
@@ -2,12 +2,19 @@ package com.example.w1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@RestController
 public class W1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(W1Application.class, args);
 	}
-
+	@GetMapping("/")
+	public String sm(){
+		return "Hello world";
+	}
 }
